cc56a27045cfb27808c1349d3ba03c9a4a5345a6
LaimaLP <pileliene.laima@gmail.com>
2024-01-10 21:59:03 +0200

    initial files

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+require '../vendor/autoload.php';
+define('ROOT', __DIR__ . '/../'); //rodo kur musu visi faila sudeti
+define('URL', 'http://bank.test'); //rodo, koks adresiukas
+ echo "labas";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+</body>
+</html>
