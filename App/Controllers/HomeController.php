6cf59ea47c4c520587e91f1687d40a4224246843
LaimaLP <pileliene.laima@gmail.com>
2024-01-22 01:08:23 +0200

    cleaned home controller

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -4,15 +4,12 @@ namespace Bank\App\Controllers;
 
 use Bank\App\App;
 
-
 class HomeController
 {
     public function index()
     {
-        $greet = ['labas','hello', 'bonjure'];
         //i templeita perduodame data
-        return App::view('test',[
-            'text' => $greet[rand(0, count($greet)-1)],
+        return App::view('home',[
             'title' => "Future Bank"
             
         ]);
